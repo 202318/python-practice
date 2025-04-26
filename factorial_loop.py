80e524b8f4a0d15ef11a7e8b32d799516a09ba66
202318 <kendijpk@gmail.com>
2025-04-26 21:11:52 +0300

    Update factorial_loop.py

--- factorial_loop.py
+++ factorial_loop.py
@@ -1,1 +1,9 @@
+def factorial_loop(n):
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial *= i
+    return factorial 
 
+number = 5
+result = factorial_loop(number)
+print(f"Factorial of {number} is {result}")

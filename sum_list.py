fdc482dfba7c8ed8976ff8e2479a0f289ba04307
202318 <kendijpk@gmail.com>
2025-04-26 21:02:44 +0300

    Update sum_list.py

--- sum_list.py
+++ sum_list.py
@@ -1,1 +1,9 @@
+def sum_list(numbers)
+    total = 0
+    for num in numbers:
+        total += num
+    return total
 
+sample_list = [1, 2, 3, 4, 5]
+result = sum_list(sample_list)
+print("Sum of list", result)

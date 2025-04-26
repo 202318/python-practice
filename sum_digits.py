96b6196d92416d4a4e257569f26558bc07b36678
202318 <kendijpk@gmail.com>
2025-04-26 21:27:39 +0300

    Update sum_digits.py

--- sum_digits.py
+++ sum_digits.py
@@ -1,1 +1,11 @@
+def sum_of_digits(number):
+    total = 0
+    while number > 0:
+        digit = number % 10 
+        total += digit
+        number //=10
+    return total
 
+num = 1234
+result = sum_of_digits(num)
+print(f"Sum of digits of {num} is {result}")

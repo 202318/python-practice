e4f5664e56db2eec0fa01eb8e8916c994e3b001d
202318 <kendijpk@gmail.com>
2025-04-26 21:15:39 +0300

    Update reverse_string.py

--- reverse_string.py
+++ reverse_string.py
@@ -1,1 +1,9 @@
+def reverse_string(word):
+    reversed_word = ''
+    for char in word:
+        reversed_word = char + reversed_word
+    return reversed_word
 
+text = "hello"
+result = reverse_string(text)
+print("Reversed string:", result)
